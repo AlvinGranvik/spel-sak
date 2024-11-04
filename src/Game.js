17436e910f31bb71efbb41e3df22e7f5f1c4405a
Alvin Granvik <118818910+AlvinGranvik@users.noreply.github.com>
2024-11-04 11:11:56 +0100

    skibidi update

---
+++ src/Game.js
@@ -0,0 +1,28 @@
+import GameObject from "./GameObject.js"
+
+export default class Game {
+  constructor(width, height) {
+    this.width = width
+    this.height = height
+    this.gameObjects = [
+      new GameObject(this, 0, 100, 20, 20, '#f00', 100),
+      new GameObject(this, 0, 200, 20, 20, '#0f0', 200),
+      new GameObject(this, 0, 300, 20, 20, '#00f', 300)
+    ]
+  }
+
+  
+
+  //Uppdaterar objekten så att de kan röra på sig
+  update(deltaTime) {
+    this.gameObjects.forEach(gameObject => {
+      gameObject.update(deltaTime)
+    })
+  }
+
+  draw(ctx) {
+    this.gameObjects.forEach(gameObject => {
+      gameObject.draw(ctx)
+    })
+  }
+}
